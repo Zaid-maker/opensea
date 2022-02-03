338dd3dcf220b6984d932eed6633e6dfc8cde627
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-02-03 19:56:38 +0500

    Add Main

---
+++ pages/collections/[collectionid].js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, useMemo } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { useWeb3 } from '@3rdweb/hooks'
+import { ThirdwebSDK } from '@3rdweb/sdk'
+import Header from '../../components/Header'
+import { CgWebsite } from 'react-icons/cg'
+import { AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai'
+import { HiDotsVertical } from 'react-icons/hi'
+
+const style = {
+  bannerImageContainer: `h-[20vh] w-screen overflow-hidden flex justify-center items-center`,
+  bannerImage: `w-full object-cover`,
+  infoContainer: `w-screen px-4`,
+  midRow: `w-full flex justify-center text-white`,
+  endRow: `w-full flex justify-end text-white`,
+  profileImg: `w-40 h-40 object-cover rounded-full border-2 border-[#202225] mt-[-4rem]`,
+  socialIconsContainer: `flex text-3xl mb-[-2rem]`,
+  socialIconsWrapper: `w-44`,
+  socialIconsContent: `flex container justify-between text-[1.4rem] border-2 rounded-lg px-2`,
+  socialIcon: `my-2`,
+  divider: `border-r-2`,
+  title: `text-5xl font-bold mb-4`,
+  createdBy: `text-lg mb-4`,
+  statsContainer: `w-[44vw] flex justify-between py-4 border border-[#151b22] rounded-xl mb-4`,
+  collectionStat: `w-1/4`,
+  statValue: `text-3xl font-bold w-full flex items-center justify-center`,
+  ethLogo: `h-6 mr-2`,
+  statName: `text-lg w-full text-center mt-1`,
+  description: `text-[#8a939b] text-xl w-max-1/4 flex-wrap mt-4`,
+}
+
+const Collection = () => {
+  const router = useRouter()
+  console.log(router.query)
+  console.log(router.query.collectionId)
+  return (
+    <Link href="/">
+      <h2>{router.query.collectionId}</h2>
+    </Link>
+  )
+}
+
+export default Collection
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Hero />
+    </>
   )
 }
