420bd70566b995c309fd6d87b957a499ad060fe1
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-02-03 17:25:41 +0500

    Navbar :tada:

---
+++ components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import openseaLogo from '../assets/opensea.png'
+
+// Tailwind CSS
+const style = {
+  wrapper: `bg-[#04111d] w-screen px-[1.2rem] py-[0.8rem] flex `,
+  logoContainer: `flex items-center cursor-pointer`,
+  logoText: ` ml-[0.8rem] text-white font-semibold text-2xl`,
+  searchBar: `flex flex-1 mx-[0.8rem] w-max-[520px] items-center bg-[#363840] rounded-[0.8rem] hover:bg-[#4c505c]`,
+  searchIcon: `text-[#8a939b] mx-3 font-bold text-lg`,
+  searchInput: `h-[2.6rem] w-full border-0 bg-transparent outline-0 ring-0 px-2 pl-0 text-[#e6e8eb] placeholder:text-[#8a939b]`,
+  headerItems: ` flex items-center justify-end`,
+  headerItem: `text-white px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer`,
+  headerIcon: `text-[#8a939b] text-3xl font-black px-4 hover:text-white cursor-pointer`,
+}
+
+const Header = () => {
+  return (
+    <div className={style.wrapper}>
+      <Link href="/">
+        <div className={style.logoContainer}>
+          <Image src={openseaLogo} width={40} height={40} />
+          <div className={style.logoText}>OpenSea</div>
+        </div>
+      </Link>
+      <div className={style.searchBar}></div>
+    </div>
+  )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <p>hmm</p>
+    <Header />
   )
 }
